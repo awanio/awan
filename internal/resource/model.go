52c2b534449e82ee177930ad87ee9f77479eea6e
Iskandar Soesman <k4ndar@yahoo.com>
2020-10-13 02:00:32 +0700

    add resource domain

---
+++ internal/resource/model.go
@@ -0,0 +1,12 @@
+package resource
+
+import (
+	"github.com/awanio/awan/pkg/database"
+	"github.com/gofrs/uuid"
+)
+
+// Service data of struct
+type Service struct {
+	database.BaseModel
+	ID uuid.UUID `gorm:"type:uuid;primary_key;"`
+}
